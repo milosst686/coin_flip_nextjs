74c372f8835542c388c5291dab9a27ac3374faaa
milosst686 <milosst686@hotmail.com>
2023-03-30 21:19:50 +0200

    pull #2
    I finished everything from yesterday and also added a logo

---
+++ pages/common/components/NavBarItem/NavBarItem.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import React from 'react';
+
+interface NavBarInterface{
+    text: string;
+    isActive?: boolean;
+    isSoon?: boolean;
+}
+
+export default function NavBarItem({text,isActive,isSoon}:NavBarInterface ) {
+  
+
+  return (
+  <div className="py-1 px-2">
+    <div className={clsx("flex items-center", !isSoon && " hover:bg-back-100 rounded-full cursor-pointer")}>
+      <div className={clsx("h-4 w-4 rounded-full mx-2 ", isActive ? "bg-accent-200" : "bg-ui-100")}> </div>
+      <p className={clsx(isActive ? "text-accent-200 mx-2" : isSoon ? "text-font-200 mx-2" : "text-white mx-2")}>{text}</p>
+      <button className= {clsx(isSoon? " rounded-full bg-ui-200 w-12 text-font-200" : "hidden")}>Soon</button>     
+    </div>
+  </div>
+  )
+}
