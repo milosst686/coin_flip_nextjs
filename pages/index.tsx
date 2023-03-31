4f8e97c21feb248d29e016ceef9eef9e0309aa57
milosst686 <milosst686@hotmail.com>
2023-03-31 13:03:54 +0200

    pull #3
    I tried to recreate right side. added image, and 4 buttons on top of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { clsx } from 'clsx';
 import NavBarItem from './common/components/NavBarItem/NavBarItem';
 import Image from 'next/image';
 import logoImg from './common/components/Logo/logo.png';
+import bgImage from './common/components/Logo/coinflip_bg.jpeg';
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
       </Head>
       <main>
       <nav >
-      <div className=" bg-ui-200 ">
-       <div className={clsx(" bg-ui-300 h-screen w-64 shadow-black  shadow-2xl")}>
+      <div className="flex bg-ui-200  ">
+       <div className={clsx(" bg-ui-300 h-screen w-[20%] shadow-black  shadow-2xl")}>
         <div className="flex items-center px-4">
           <Image src={logoImg} alt="/" width='50'height='50' />
           <p className="pt-4 text-red-400 font-bold">BWS GAMES</p>
@@ -37,6 +38,31 @@ export default function Home() {
               <NavBarItem text="Dividends"  isSoon/>
             </div>
         </div>
+       </div>
+       <div className=" w-full pl-4 py-6">
+      <div className=" w-full flex justify-between">
+        <div >
+        <button className=" py-2 uppercase rounded-full border-font-100 border-2 w-32">
+          <span className="text-font-100 text-sm"> Swap $Coins</span>  
+        </button>
+        </div>
+        <div className="flex items-center pr-12 ">
+          <span className="rounded-full bg-ui-100 text-white px-5 ">sdadasd</span>
+          <span className="rounded-full bg-ui-100 text-white px-2">sdadasd</span>
+          <button className="rounded-full bg-accent-200 px-2 text-white">Connect wallet</button>
+        </div>
+      </div>
+      <div className="pr-4 py-8">
+        <div >
+          <Image 
+          src={bgImage}
+          alt="BWS resort"
+          className=" rounded-xl object-cover w-[100%]"
+          />
+          <NavBarItem text="Analytics"  />          
+
+        </div>
+      </div>
        </div>
       </div>
       </nav>
